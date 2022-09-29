d4ef78f4b08b5a98b7635bc33932a9c867b992f0
Ashutosh Singh <hiashutoshkumarsingh@gmail.com>
2022-09-29 10:35:11 +0530

    Add heartbeat animation

--- lib/home.dart
+++ lib/home.dart
@@ -11,26 +11,99 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> with TickerProviderStateMixin {
-  late final AnimationController _heartScaleController;
-  late final Animation<Size> _heartScaleAnimation;
+  late final AnimationController _heartPressedScaleController;
+  late final Animation<Size> _heartPressedScaleAnimation;
+  late final Animation<Color?> _heartBackgroundColorAnimation;
+  late final Animation<Color?> _heartBorderColorAnimation;
+  late final ColorTween _heartBorderColorTween;
+  late final ColorTween _heartBackgroundColorTween;
+  late final AnimationController _heartReleasedScaleController;
+  late final Animation<Color?> _heartBackgroundConstantColorAnimation;
+  late final ColorTween _heartConstantBorderColorTween;
+  late final Animation<Color?> _heartConstantBorderColorAnimation;
+  late final ColorTween _heartConstantBackgroundColorTween;
+  late final Animation<Size> _heartReleasedScaleAnimation;
+  late bool _isPressed;
 
   @override
   void initState() {
     super.initState();
-    _heartScaleController = AnimationController(
+    _isPressed = true;
+    _heartPressedScaleController = AnimationController(
       vsync: this,
       duration: const Duration(milliseconds: 100),
     );
-    _heartScaleAnimation = Tween<Size>(
+
+    _heartReleasedScaleController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 100),
+    );
+
+    _heartPressedScaleAnimation = Tween<Size>(
       begin: const Size(100, 110),
       end: const Size(80, 90),
-    ).animate(_heartScaleController);
+    ).animate(_heartPressedScaleController);
+
+    _heartReleasedScaleAnimation = Tween<Size>(
+      begin: const Size(80, 90),
+      end: const Size(100, 110),
+    ).animate(_heartReleasedScaleController);
+
+    _heartConstantBackgroundColorTween = ColorTween(
+      begin: Constants.black,
+      end: Constants.black,
+    );
+
+    _heartBackgroundColorTween = ColorTween(
+      begin: Constants.black,
+      end: Constants.green,
+    );
+
+    _heartBackgroundConstantColorAnimation = _heartConstantBackgroundColorTween
+        .animate(_heartPressedScaleController);
+
+    _heartBackgroundColorAnimation =
+        _heartBackgroundColorTween.animate(_heartReleasedScaleController);
+
+    _heartBorderColorTween = ColorTween(
+      begin: Colors.white,
+      end: Constants.black,
+    );
+
+    _heartBorderColorAnimation =
+        _heartBorderColorTween.animate(_heartPressedScaleController);
+
+    _heartConstantBorderColorTween = ColorTween(
+      begin: Colors.black,
+      end: Constants.black,
+    );
+
+    _heartConstantBorderColorAnimation =
+        _heartConstantBorderColorTween.animate(_heartReleasedScaleController);
   }
 
   @override
   void dispose() {
-    _heartScaleController.dispose();
+    _heartPressedScaleController.dispose();
     super.dispose();
+  }
+
+  Animation<Color?> _getBackGroundAnimation() {
+    return _isPressed
+        ? _heartBackgroundConstantColorAnimation
+        : _heartBackgroundColorAnimation;
+  }
+
+  Animation<Color?> _getBorderColorAnimation() {
+    return _isPressed
+        ? _heartBorderColorAnimation
+        : _heartConstantBorderColorAnimation;
+  }
+
+  Animation<Size> _getHeartScaleAnimation() {
+    return _isPressed
+        ? _heartPressedScaleAnimation
+        : _heartReleasedScaleAnimation;
   }
 
   @override
@@ -39,19 +112,32 @@ class _HomeState extends State<Home> with TickerProviderStateMixin {
       backgroundColor: Constants.black,
       body: GestureDetector(
         onTap: () {
-          _heartScaleController
-              .forward()
-              .then((value) => _heartScaleController.reverse());
+          _heartPressedScaleController.forward().then((value) {
+            setState(() {
+              _isPressed = false;
+            });
+            _heartReleasedScaleController.forward();
+          });
         },
         child: Center(
           child: AnimatedBuilder(
-            animation: _heartScaleAnimation,
+            animation: _getHeartScaleAnimation(),
             builder: (BuildContext context, Widget? child) {
-              return Heart(
-                backgroundColor: Constants.black,
-                borderColor: Colors.white,
-                height: _heartScaleAnimation.value.height,
-                width: _heartScaleAnimation.value.width,
+              return AnimatedBuilder(
+                animation: _getBackGroundAnimation(),
+                builder: (BuildContext context, Widget? child) {
+                  return AnimatedBuilder(
+                    animation: _getBorderColorAnimation(),
+                    builder: (BuildContext context, Widget? child) {
+                      return Heart(
+                        backgroundColor: _getBackGroundAnimation().value!,
+                        borderColor: _getBorderColorAnimation().value!,
+                        height: _getHeartScaleAnimation().value.height,
+                        width: _getHeartScaleAnimation().value.width,
+                      );
+                    },
+                  );
+                },
               );
             },
           ),
@@ -60,6 +146,3 @@ class _HomeState extends State<Home> with TickerProviderStateMixin {
     );
   }
 }
-
-// 0xff1DB954 green color
-// 0xff191414 black color
